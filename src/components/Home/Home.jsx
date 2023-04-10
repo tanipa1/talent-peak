64dc9c869935c5c4a5a4738956d8835bf2994b2a
tanipa1 <tania35.291@gmail.com>
2023-04-11 03:26:13 +0600

    Blog Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Header from '../Header/Header';
+import JobCategory from '../JobCategory/JobCategory';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+    const jobCat = useLoaderData();
+    console.log(jobCat);
+    const {Jobs, title, img} = jobCat;
     return (
         <div>
             <div>
                 <Header></Header>
+                <h2>Name: {Jobs}</h2>
+            <p>{title}</p>
             </div>
         </div>
     );
--- src/components/JobCategory/JobCategory.jsx
+++ src/components/JobCategory/JobCategory.jsx
@@ -4,9 +4,11 @@ import { useLoaderData } from 'react-router-dom';
 const JobCategory = () => {
     const jobCat = useLoaderData();
     console.log(jobCat);
+    const {Jobs, title, img} = jobCat;
     return (
         <div>
-            
+            <h2>Name: {Jobs}</h2>
+            <p>{title}</p>
         </div>
     );
 };
