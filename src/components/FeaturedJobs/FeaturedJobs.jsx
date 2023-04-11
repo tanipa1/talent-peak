976cb9404ddf4aa07544f913593f28a0b4797aa2
tanipa1 <tania35.291@gmail.com>
2023-04-11 12:12:50 +0600

    featured Card Style

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 import Jobs from '../Jobs/Jobs';
+import './featuredJobs.css';
 
-const FeaturedJobs = ({featured}) => {
+const FeaturedJobs = ({ featured }) => {
 
     return (
-        <div>
-            {
-                featured.map(jobs => 
-                <Jobs
-                    key={jobs.id}
-                    jobs={jobs}
-                ></Jobs>)
-            }
+        <div className='featured'>
+            <div className='featured-details'>
+                <h2>Featured Jobs</h2>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div className='featured-jobs'>
+                {
+                    featured.map(jobs =>
+                        <Jobs
+                            key={jobs.id}
+                            jobs={jobs}
+                        ></Jobs>)
+                }
+            </div>
+            <div className='all-btn'>
+                <button className='see-all'>See All Jobs</button>
+            </div>
         </div>
     );
 };
